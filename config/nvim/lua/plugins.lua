edce3a138fe07a3265f8d2932428fc888432aeff
ibra-kdbra <git-repo-develop@outlook.com>
2025-05-03 17:31:28 +0300

    nvim: Witch keys

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -44,7 +44,7 @@ return {
         { "<leader>B", desc = "Buffer Next", icon = "󰒭" },
 
         { "<leader>o", desc = "Explorer Files", icon = "" },
-        { "<leader>O", desc = "Explorer Files Vsplit", icon = "󱉆" },
+        { "<leader>O", desc = "Explorer Files", icon = "󱉆" },
 
         { "<leader>Q", desc = "Delete Buffer", icon = "󱂦" },
         { "<leader>S", desc = "Save Buffer", icon = "" },
@@ -53,14 +53,16 @@ return {
         { "<leader>T", desc = "Terminal Vsplit", icon = "" },
 
         { "<leader>v", desc = "Window Vsplit", icon = "" },
-        { "<leader>V", desc = "Window Buffer Vsplit", icon = "" },
+        { "<leader>V", desc = "Tab Split", icon = "" },
+
+        { "<leader>W", desc = "Save Buffer", icon = "" },
+        { "<leader>X", desc = "Delete Buffer", icon = "󱂦" },
 
         { "<leader>/", desc = "NoHlSearch", icon = "" },
         { "<leader><space>", desc = "EasyMotion", icon = "" },
 
         { "<leader>c", group = " NerdCommenter", icon = "" },
-        { "<leader>f", group = " Fzf", icon = "󱎸" },
-      })
+        { "<leader>f", group = " Fzf", icon = "󱎸" },      })
     end,
     },
   
