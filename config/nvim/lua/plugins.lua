14154ee545a665e9b5fa638373038b7c7be494b0
ibra-kdbra <git-repo-develop@outlook.com>
2025-05-03 16:43:45 +0300

    nvim: which-key desc

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -27,13 +27,41 @@ return {
       opts = {
         -- Main settings
         preset = "helix", -- Choose the style: "classic", "modern", "helix"
-        delay = 550, -- Delay before showing the window (in milliseconds)
+        delay = 350, -- Delay before showing the window (in milliseconds)
         -- Window settings
         win = {
           border = "rounded", -- Border style: "none", "single", "double", "rounded"
           padding = { 1, 2 }, -- Padding inside the window [top/bottom, left/right]
         },
       },
+      config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      
+      -- Registering prefixes
+      wk.add({
+        { "<leader>c", group = " NerdCommenter" },
+        { "<leader>f", group = " Fzf" },
+
+        { "<leader>b", desc = "Buffer Next" },
+        { "<leader>B", desc = "Buffer Prev" },
+
+        { "<leader>o", desc = "Explorer Files" },
+        { "<leader>O", desc = "Explorer Files Vsplit" },
+
+        { "<leader>Q", desc = "Delete Buffer" },
+        { "<leader>S", desc = "Save Buffer" },
+
+        { "<leader>t", desc = "Terminal Split" },
+        { "<leader>T", desc = "Terminal Vsplit" },
+
+        { "<leader>v", desc = "Window Vsplit" },
+        { "<leader>V", desc = "Window Buffer Vsplit" },
+
+        { "<leader>/", desc = "NoHlSearch" },
+        { "<leader><space>", desc = "EasyMotion" },
+      })
+    end,
     },
   
   }  
